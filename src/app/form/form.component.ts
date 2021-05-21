5e44326e67a298e9fa31f8ec85479632a4262391
Vahan Stepanyan <vahan.st@live.com>
2021-05-21 15:38:45 +0400

    Fixes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,19 +12,27 @@ export class FormComponent {
 
   @Output() popupOpened: EventEmitter<object> = new EventEmitter();
   @Output() timerFormSubmit: EventEmitter<object> = new EventEmitter();
-  isHidden: boolean = false;
   mainForm = new FormGroup({});
 
   get timerName(): string {
     return this.mainForm.value?.timerForm?.name;
   }
-
+  get timerDescription(): string {
+    return this.mainForm.value?.timerForm?.description;
+  }
+  get timerComment(): string {
+    return this.mainForm.value?.timerForm?.comment;
+  }
+  get timerCount(): number {
+    return this.mainForm.value?.timerForm?.timerCount;
+  }
   closeDialog(): void{
     this.popupOpened.emit();
   }
 
   onSubmit(): void {
     this.popupOpened.emit();
-    this.timerFormSubmit.emit({name : this.timerName});
+    this.timerFormSubmit.emit({name : this.timerName, description: this.timerDescription, comment: this.timerComment,
+      count: this.timerCount});
   }
 }
